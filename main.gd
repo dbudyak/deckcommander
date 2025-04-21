3ee1083ba834c69f5f54e404cecee4c0b4d80b51
Dima Budiak <dima@MacBookPro.lan>
2025-04-21 22:35:35 +0300

    go up works

--- main.gd
+++ main.gd
@@ -1,10 +1,10 @@
 extends Control
 
-@onready var left_panel = $VBoxContainer/PanelsContainer/LeftPanel
-@onready var right_panel = $VBoxContainer/PanelsContainer/RightPanel
-@onready var status_label = $VBoxContainer/StatusLabel
+@onready var left_panel : VBoxContainer = $VBoxContainer/PanelsContainer/LeftPanel
+@onready var right_panel : VBoxContainer = $VBoxContainer/PanelsContainer/RightPanel
+@onready var status_label : HBoxContainer = $VBoxContainer/StatusLabel
 
-var active_panel = "left"  # or "right"
+var active_panel : String= "left"  # or "right"
 
 func _ready():
 	left_panel.set_path(OS.get_system_dir(OS.SYSTEM_DIR_DESKTOP))
@@ -19,6 +19,8 @@ func _unhandled_input(event):
 		toggle_active_panel()
 	elif event.is_action_pressed("ui_cancel"):
 		get_active_panel().go_up()
+	elif event.is_action_pressed("ui_text_backspace"):
+		get_active_panel().go_up()
 
 func toggle_active_panel():
 	if active_panel == "left":
@@ -32,5 +34,5 @@ func toggle_active_panel():
 		left_panel.is_focused = true
 		left_panel.grab_focus()
 
-func get_active_panel():
-	return active_panel == left_panel if "left" else right_panel
+func get_active_panel() -> VBoxContainer:
+	return left_panel if active_panel == "left" else right_panel
